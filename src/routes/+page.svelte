c7919646d4f648b9591db6e572677e54981cc769
Jeff Allen <jeffallen89@gmail.com>
2024-05-14 11:33:50 -0400

    add example scrolly content json

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,33 @@
 import Top from "$lib/TopSofC.svelte";
 import "../assets/styles.css";
 
+let scrollyContent = [
+	{
+		"x": -79.40896,
+		"y": 43.66485,
+		"zoom": 16,
+		"text": "Hello I am some text!",
+	},
+	{
+		"x": -79.40744,
+		"y": 43.63588,
+		"zoom": 15,
+		"text": "Hello I am some more text!",
+	},
+	{
+		"x": -79.48587,
+		"y": 43.64944,
+		"zoom": 17,
+		"text": "Hello I am some more more text!",
+	},
+	{
+		"x": -79.34503,
+		"y": 43.67764,
+		"zoom": 15,
+		"text": "Hello text I am!",
+	}
+]
+
 </script>
 
 
